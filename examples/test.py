db4755c5ec495d0ec7112203d2de08ae19a99913
Sebastian Hocke <92033636+sebh96@users.noreply.github.com>
2024-05-29 13:17:01 +0200

    mnist update

--- examples/test.py
+++ examples/test.py
@@ -1,2 +1,52 @@
-def test():
-    print('this is a test function')
+import tensorflow as tf
+import numpy as np
+from clearml import Task
+import keras
+import os
+import tempfile
+
+def mnist():
+    '''
+    Training on the MNIST dataset
+    '''   
+    batch_size = 128
+    epochs = 30
+    
+    num_classes = 10
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    
+    x_train=x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
+    x_train=x_train / 255.0
+    x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
+    x_test=x_test/255.0
+    y_train = tf.one_hot(y_train.astype(np.int32), depth=10)
+    y_test = tf.one_hot(y_test.astype(np.int32), depth=10)
+    
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(32, (5,5), padding='same', activation='relu', input_shape=(28,28,1)),
+        tf.keras.layers.Conv2D(32, (5,5), padding='same', activation='relu'),
+        tf.keras.layers.MaxPool2D(),
+        tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.Conv2D(64, (3,3), padding='same', activation='relu'),
+        tf.keras.layers.Conv2D(64, (3,3), padding='same', activation='relu'),
+        tf.keras.layers.MaxPool2D(strides=(2,2)),
+        tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(num_classes, activation='softmax')
+        ])
+    
+    model.compile(optimizer=tf.keras.optimizers.RMSprop(epsilon=1e-08), loss='categorical_crossentropy', metrics=['acc'])
+    
+    output_folder = os.path.join(tempfile.gettempdir(), 'keras_example')
+    board = keras.callbacks.TensorBoard(histogram_freq=1, log_dir=output_folder, write_images=False)
+    model_store = keras.callbacks.ModelCheckpoint(filepath=os.path.join(output_folder, 'weight.{epoch}.keras'))
+    
+    # Training
+    history = model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              validation_split=0.1,
+              callbacks=[board, model_store])
