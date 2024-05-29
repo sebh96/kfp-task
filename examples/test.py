8c2d3de777555b5d1ad4cafc5ead8601c5e0ec61
Sebastian Hocke <92033636+sebh96@users.noreply.github.com>
2024-05-29 13:20:23 +0200

    remove clearml

--- examples/test.py
+++ examples/test.py
@@ -1,6 +1,5 @@
 import tensorflow as tf
 import numpy as np
-from clearml import Task
 import keras
 import os
 import tempfile
@@ -11,18 +10,18 @@ def mnist():
     '''   
     batch_size = 128
     epochs = 30
-    
+
     num_classes = 10
     mnist = tf.keras.datasets.mnist
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    
+
     x_train=x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1)
     x_train=x_train / 255.0
     x_test = x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)
     x_test=x_test/255.0
     y_train = tf.one_hot(y_train.astype(np.int32), depth=10)
     y_test = tf.one_hot(y_test.astype(np.int32), depth=10)
-    
+
     model = tf.keras.models.Sequential([
         tf.keras.layers.Conv2D(32, (5,5), padding='same', activation='relu', input_shape=(28,28,1)),
         tf.keras.layers.Conv2D(32, (5,5), padding='same', activation='relu'),
@@ -37,13 +36,13 @@ def mnist():
         tf.keras.layers.Dropout(0.5),
         tf.keras.layers.Dense(num_classes, activation='softmax')
         ])
-    
+
     model.compile(optimizer=tf.keras.optimizers.RMSprop(epsilon=1e-08), loss='categorical_crossentropy', metrics=['acc'])
-    
+
     output_folder = os.path.join(tempfile.gettempdir(), 'keras_example')
     board = keras.callbacks.TensorBoard(histogram_freq=1, log_dir=output_folder, write_images=False)
     model_store = keras.callbacks.ModelCheckpoint(filepath=os.path.join(output_folder, 'weight.{epoch}.keras'))
-    
+
     # Training
     history = model.fit(x_train, y_train,
               batch_size=batch_size,
